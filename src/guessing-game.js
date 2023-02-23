493c176554603ad5b0d0208d28c4b6062d470a43
Viktoriya <kruckovaviktoria421@gmail.com>
2023-02-23 16:04:50 +0300

    init: create new function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.start = null;
+        this.end = null;
+        this.middle = null;
+    }
 
     setRange(min, max) {
-
+        this.start = min;
+        this.end = max;
     }
 
     guess() {
-
+        this.middle = Math.round((this.start + this.end) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.end = this.middle;
     }
 
     greater() {
-
+        this.start = this.middle;
     }
 }
-
 module.exports = GuessingGame;
